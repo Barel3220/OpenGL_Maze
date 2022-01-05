27f208a23b4b3b227cf6e4d0fc52433b02fc1867
Barel Hatuka <barelhatuka@gmail.com>
2022-01-05 14:15:21 +0200

    Drawing outer walls and start point

--- OpenGL_Maze/Main.cpp
+++ OpenGL_Maze/Main.cpp
@@ -1,18 +1,72 @@
 #include <gl/glut.h>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
+
+// declaring variables
+const int HEIGHT = 600;
+const int WIDTH = 600;
+
+// declaring const ints for numbering the maze
+const int SPACE = 0;
+const int START = 1;
+const int WALL = 2;
+
+const int MAZE_SIZE = 200;
+const double SQAURE_SIZE = 2.0 / MAZE_SIZE;
+
+int _maze[MAZE_SIZE][MAZE_SIZE];
+
+void init()
+{
+	// clear maze
+	for (int row = 0; row < MAZE_SIZE; row++)
+		for (int column = 0; column < MAZE_SIZE; column++)
+			_maze[row][column] = 0;
+
+	// start point
+	_maze[MAZE_SIZE / 2][MAZE_SIZE / 2] = 1;
+
+	// drawing outer walls
+	for (int i = 0; i < MAZE_SIZE; i++)
+		// left // right // top // bottom
+		_maze[i][0] = _maze[i][MAZE_SIZE - 1] = _maze[0][i] = _maze[MAZE_SIZE - 1][i] = 2;
+	
+}
 
 // this function will be called by GLUT every time the window needs to be painted.
 void renderDisplay()
 {	
+	// indicates the buffers currently enabled for color writing.
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	// drawing triangle
-	glBegin(GL_TRIANGLES);
-	glVertex3f(-.5, -.5, .0);
-	glVertex3f(.5, .0, .0);
-	glVertex3f(.0, .5, .0);
-	glEnd();
+	// switch case for indicating what color to draw the square
+	for (int row = 0; row < MAZE_SIZE; row++)
+		for (int column = 0; column < MAZE_SIZE; column++)
+		{
+			switch (_maze[row][column])
+			{
+			case SPACE:
+				glColor3d(1, 1, 1); // white;
+				break;
+			case START:
+				glColor3d(0, 0, 1); // blue;
+				break;
+			case WALL:
+				glColor3d(.4, 0, 0); // dark red;
+				break;
+			default:
+				break;
+			}
+			// drawing square for every color
+			glBegin(GL_POLYGON);
+			glVertex2d(column * SQAURE_SIZE - 1 - SQAURE_SIZE / 2, row * SQAURE_SIZE - 1 + SQAURE_SIZE / 2);
+			glVertex2d(column * SQAURE_SIZE - 1 + SQAURE_SIZE / 2, row * SQAURE_SIZE - 1 + SQAURE_SIZE / 2);
+			glVertex2d(column * SQAURE_SIZE - 1 + SQAURE_SIZE / 2, row * SQAURE_SIZE - 1 - SQAURE_SIZE / 2);
+			glVertex2d(column * SQAURE_SIZE - 1 - SQAURE_SIZE / 2, row * SQAURE_SIZE - 1 - SQAURE_SIZE / 2);
+			glEnd();
+		}
 
 	glutSwapBuffers();
 }
@@ -24,7 +78,7 @@ void main(int argc, char* argv[])
 	glutInit(&argc, argv);
 	// top left corner position
 	glutInitWindowPosition(200, 100);
-	glutInitWindowSize(600, 600);
+	glutInitWindowSize(WIDTH, HEIGHT);
 	// the mode parameter is a boolean combination (OR bit wise) of the possible values in the GLUT library
 	// must specify the color mode, number and type of buffers.
 	// here the color mode is RGB and the buffer is a double buffer which required for smooth animation.
@@ -34,6 +88,9 @@ void main(int argc, char* argv[])
 	
 	// register callbacks // refresh function
 	glutDisplayFunc(renderDisplay);
+
+	// sketching the maze
+	init();
 
 	// start GLUT event processing cycle
 	glutMainLoop();
